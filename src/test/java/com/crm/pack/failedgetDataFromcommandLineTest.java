f35def68f8d2f266ab1193b043d45661b75b3419
Lenovo <Lenovo@DESKTOP-SR3NH7B>
2021-12-15 13:29:26 +0530

    7th

--- src/test/java/com/crm/pack/failedgetDataFromcommandLineTest.java
+++ src/test/java/com/crm/pack/failedgetDataFromcommandLineTest.java
@@ -2,17 +2,17 @@ package com.crm.pack;
 
 import org.testng.annotations.Test;
 
-public class failedgetDataFromcommandLineTest {
+public class failedgetDataFromcommandLineTest  {
 
 	@Test	
   public void commandlineTest() {
-	  String url = System.getProperty("url");
-	 String username = System.getProperty("username");
-	 String password = System.getProperty("password");
+	 // String url = System.getProperty("url");
+	// String username = System.getProperty("username");
+	// String password = System.getProperty("password");
 	 
-	 System.out.println(url);
-	 System.out.println(username);
-	 System.out.println(password);
+	 System.out.println("url");
+	 System.out.println("username");
+	 System.out.println("pasword ");
 	 
 	 
 	 
